b73360c584fe9aaaa18e9cc8107170ab306e1b6c
Harrison Blake <harrison.r.blake@gmail.com>
2025-04-11 13:49:40 -0400

    run linter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,4 +21,3 @@ class SessionsController < ApplicationController
     redirect_to root_path, notice: "Logged out"
   end
 end
-
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,31 @@
 class UsersController < ApplicationController
-	before_action :require_login
+  before_action :require_login
 
-	def new
-		@user = User.new()
-	end
+  def new
+    @user = User.new()
+  end
 
-	def show
-		@user = current_user
-	end
+  def show
+    @user = current_user
+  end
 
-	def create
-    	@user = User.new(user_params)
+  def create
+      @user = User.new(user_params)
 
-    	if @user.save
-      		session[:user_id] = @user.id
-      		redirect_to profile_edit_path
-    	else
-      		flash.now[:alert] = "There was a problem with your signup."
-      		render :new, status: :unprocessable_entity
-    	end
-  	end
+      if @user.save
+          session[:user_id] = @user.id
+          redirect_to profile_edit_path
+      else
+          flash.now[:alert] = "There was a problem with your signup."
+          render :new, status: :unprocessable_entity
+      end
+    end
 
   def edit
-  	@user = current_user
-	end
+    @user = current_user
+  end
 
-	def update
+  def update
     @user = current_user
 
     if @user.update(user_params)
@@ -43,5 +43,5 @@ private
     end
 
     def require_login
-    	redirect_to login_path unless current_user
-  	end
+      redirect_to login_path unless current_user
+    end
